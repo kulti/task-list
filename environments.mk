8d5833f56aab26ea1f086915ee2ab9f76945244f
Aleksey Bakin <alexey.bakin@gmail.com>
2020-06-22 17:40:37 +0300

    feat infra: deploy from docker hub

--- environments.mk
+++ environments.mk
@@ -4,24 +4,15 @@ up-tl-prod-deps: build-docker-tl-proxy build-docker-tl-front build-docker-tl-ser
 up-tl-dev-deps: build-docker-tl-proxy build-docker-tl-front build-docker-tl-live-reload build-docker-tl-migrate
 
 $(addprefix up-tl-, $(ENVIRONMENTS)): up-tl-%: % up-tl-%-deps
-	cd deployments && \
-	source $<.env && \
-	docker-compose -p $< -f docker-compose.yaml -f docker-compose.$<.yaml run db_migrations up && \
-	docker-compose -p $< -f docker-compose.yaml -f docker-compose.$<.yaml up -d
+	./scripts/deploy.sh $< up
 
 $(addprefix down-tl-, $(ENVIRONMENTS)): down-tl-%: %
-	cd deployments && \
-	source $<.env && \
-	docker-compose -p $< -f docker-compose.yaml -f docker-compose.$<.yaml down
+	./scripts/deploy.sh $< down
 
 $(addprefix ps-tl-, $(ENVIRONMENTS)): ps-tl-%: %
-	cd deployments && \
-	source $<.env && \
-	docker-compose -p $< -f docker-compose.yaml -f docker-compose.$<.yaml ps
+	./scripts/deploy.sh $< ps
 
 $(addprefix logs-tl-, $(ENVIRONMENTS)): logs-tl-%: %
-	cd deployments && \
-	source $<.env && \
-	docker-compose -p $< -f docker-compose.yaml -f docker-compose.$<.yaml logs -f
+	./scripts/deploy.sh $< logs
 
 .PHONY: $(ENVIRONMENTS)
---
+++ scripts/deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+env=$1
+action=$2
+
+cd deployments
+[ -f ~/.tl/${env}.env ] && source ${env}.env
+[ -f ~/.tl/${env}.env ] && source ~/.tl/${env}.env
+
+case $action in
+up)
+	docker-compose -p ${env} -f docker-compose.yaml -f docker-compose.${env}.yaml run db_migrations up
+	docker-compose -p ${env} -f docker-compose.yaml -f docker-compose.${env}.yaml up -d
+    ;;
+ps|down)
+	docker-compose -p ${env} -f docker-compose.yaml -f docker-compose.${env}.yaml $action
+    ;;
+logs)
+    docker-compose -p ${env} -f docker-compose.yaml -f docker-compose.${env}.yaml logs -f
+    ;;
+*)
+cat << EOF
+Usage ./scripts/deploy.sh environemnt command
+
+Commands:
+    up
+    down
+    ps
+    logs
+
+Environments:
+    dev
+    prod
+
+Config files for specific environment should be placed at ~/.tl/<environment>.env. For example:
+    export IMAGE_TAG=v0.6
+    export PROXY_PORT=2347
+EOF
+    ;;
+esac
