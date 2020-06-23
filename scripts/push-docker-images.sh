3babf174c205e0037e486e323ab154e9aed5bf3c
Aleksey Bakin <alexey.bakin@gmail.com>
2020-06-23 10:54:25 +0300

    fix infra: fix push to docker hub

--- scripts/push-docker-images.sh
+++ scripts/push-docker-images.sh
@@ -4,8 +4,6 @@ set -e
 
 git_tag=$(git describe --tags 2> /dev/null)
 if [[ ${git_tag} != "" ]]; then
-    echo -n ${DOCKER_HUB_TOKEN} | docker login --username kulti --password-stdin
-
     services="tl-proxy tl-front tl-server tl-migrate"
     for s in ${services}; do
         image_s="kulti/${s}"
@@ -14,6 +12,5 @@ if [[ ${git_tag} != "" ]]; then
         docker tag ${image_s} ${image}
         echo -n "pushing ... "
         docker push ${image}
-        echo "done"
     done
 fi
