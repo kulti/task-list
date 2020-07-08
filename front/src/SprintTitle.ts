0907e3ee7a2ed937dddff52fa6b6bdb61981f02b
Aleksey Bakin <alexey.bakin@gmail.com>
2020-07-08 23:19:22 +0300

    feat front: support sprint begin and end dates

---
+++ front/src/SprintTitle.ts
@@ -0,0 +1,49 @@
+import * as models from "./openapi_cli/model/models"
+
+export function getNewSprintOpts(): models.SprintOpts {
+    const { begin, end } = getNewSprintDates()
+    return {
+        title: formatSrpintTitle(begin, end),
+        begin: formatSrpintDate(begin),
+        end: formatSrpintDate(end),
+    };
+};
+
+function formatSrpintDate(d: Date): string {
+    return d.getUTCFullYear() + "-" + numToString(d.getMonth() + 1) + "-" + numToString(d.getDate());
+}
+
+export function buildNewSprintTitle(): string {
+    const { begin, end } = getNewSprintDates();
+    return formatSrpintTitle(begin, end);
+}
+
+export function getNewSprintDates(): { begin: Date, end: Date } {
+    const date = new Date();
+    const dayOfWeek = date.getDay();
+
+    date.setDate(date.getDate() + 1 - dayOfWeek);
+    const beginDate = date;
+    const endDate = new Date(date);
+
+    endDate.setDate(date.getDate() + 6)
+
+    return { begin: beginDate, end: endDate };
+};
+
+function formatSrpintTitle(begin: Date, end: Date): string {
+    return dateToString(begin) + " - " + dateToString(end);
+}
+
+
+function dateToString(d: Date): string {
+    return numToString(d.getDate()) + "." + numToString(d.getMonth() + 1);
+}
+
+function numToString(v: number): string {
+    let s = v.toString()
+    if (v < 10) {
+        s = "0" + s
+    }
+    return s;
+}
