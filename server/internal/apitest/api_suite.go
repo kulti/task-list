7be1f0a0d0c4ff6489554100b334f684f6751a80
Aleksey Bakin <alexey.bakin@gmail.com>
2020-07-09 23:01:02 +0300

    refactor tests: split APISuite to base and actions

--- server/internal/apitest/api_suite.go
+++ server/internal/apitest/api_suite.go
@@ -1,30 +1,5 @@
 package apitest
 
-import (
-	"context"
-	"time"
-
-	"github.com/bxcodec/faker/v3"
-	"github.com/stretchr/testify/suite"
-
-	"github.com/kulti/task-list/server/internal/generated/openapicli"
-)
-
 type APISuite struct {
-	suite.Suite
-	cli         *openapicli.APIClient
-	ctx         context.Context
-	apiURL      string
-	sprintTitle string
-	sprintDate  time.Time
-}
-
-func (s *APISuite) Init(apiURL string) {
-	apiCfg := openapicli.NewConfiguration()
-	s.cli = openapicli.NewAPIClient(apiCfg)
-	s.cli.ChangeBasePath(apiURL + "/api/v1")
-	s.ctx = context.Background()
-	s.apiURL = apiURL
-	s.sprintTitle = faker.Sentence()
-	s.sprintDate = time.Now()
+	APISuiteActions
 }
--- server/internal/apitest/api_suite_actions.go
+++ server/internal/apitest/api_suite_actions.go
@@ -1,12 +1,36 @@
 package apitest
 
 import (
+	"context"
 	"net/http"
+	"time"
+
+	"github.com/bxcodec/faker/v3"
+	"github.com/stretchr/testify/suite"
 
 	"github.com/kulti/task-list/server/internal/generated/openapicli"
 )
 
-func (s *APISuite) newSprint() {
+type APISuiteActions struct {
+	suite.Suite
+	cli         *openapicli.APIClient
+	ctx         context.Context
+	apiURL      string
+	sprintTitle string
+	sprintDate  time.Time
+}
+
+func (s *APISuiteActions) Init(apiURL string) {
+	apiCfg := openapicli.NewConfiguration()
+	s.cli = openapicli.NewAPIClient(apiCfg)
+	s.cli.ChangeBasePath(apiURL + "/api/v1")
+	s.ctx = context.Background()
+	s.apiURL = apiURL
+	s.sprintTitle = faker.Sentence()
+	s.sprintDate = time.Now()
+}
+
+func (s *APISuiteActions) newSprint() {
 	s.T().Helper()
 	opts := openapicli.SprintOpts{
 		Title: s.sprintTitle,
@@ -20,12 +44,14 @@ func (s *APISuite) newSprint() {
 	s.Require().Empty(tmpl.Tasks)
 }
 
-func (s *APISuite) checkSprintTaskList(tasks ...openapicli.RespTask) {
+func (s *APISuiteActions) checkSprintTaskList(tasks ...openapicli.RespTask) {
 	s.T().Helper()
 	s.checkTaskList(openapicli.SPRINT, s.sprintTitle, tasks...)
 }
 
-func (s *APISuite) checkTaskList(listID openapicli.ListId, listTitle string, tasks ...openapicli.RespTask) {
+func (s *APISuiteActions) checkTaskList(
+	listID openapicli.ListId, listTitle string, tasks ...openapicli.RespTask,
+) {
 	s.T().Helper()
 	taskList, resp, err := s.cli.DefaultApi.GetTaskList(s.ctx, listID)
 	s.Require().NoError(err, s.errBody(err))
@@ -40,12 +66,12 @@ func (s *APISuite) checkTaskList(listID openapicli.ListId, listTitle string, tas
 	}
 }
 
-func (s *APISuite) createSprintTask() openapicli.RespTask {
+func (s *APISuiteActions) createSprintTask() openapicli.RespTask {
 	s.T().Helper()
 	return s.createTask(openapicli.SPRINT, s.testTask())
 }
 
-func (s *APISuite) createTask(listID openapicli.ListId, task openapicli.Task) openapicli.RespTask {
+func (s *APISuiteActions) createTask(listID openapicli.ListId, task openapicli.Task) openapicli.RespTask {
 	s.T().Helper()
 	respTask, resp, err := s.cli.DefaultApi.CreateTask(s.ctx, listID, task)
 	s.Require().NoError(err, s.errBody(err))
@@ -69,12 +95,12 @@ func (s *APISuite) createTask(listID openapicli.ListId, task openapicli.Task) op
 	return respTask
 }
 
-func (s *APISuite) deleteSprintTask(taskID string) {
+func (s *APISuiteActions) deleteSprintTask(taskID string) {
 	s.T().Helper()
 	s.deleteTaskFromList(taskID, openapicli.SPRINT)
 }
 
-func (s *APISuite) deleteTaskFromList(taskID string, listID openapicli.ListId) {
+func (s *APISuiteActions) deleteTaskFromList(taskID string, listID openapicli.ListId) {
 	s.T().Helper()
 	resp, err := s.cli.DefaultApi.DeleteTask(s.ctx, listID, taskID)
 	s.Require().NoError(err, s.errBody(err))
@@ -82,7 +108,7 @@ func (s *APISuite) deleteTaskFromList(taskID string, listID openapicli.ListId) {
 	s.Require().Equal(http.StatusOK, resp.StatusCode)
 }
 
-func (s *APISuite) todoTask(taskID string) {
+func (s *APISuiteActions) todoTask(taskID string) {
 	s.T().Helper()
 	resp, err := s.cli.DefaultApi.TodoTask(s.ctx, taskID)
 	s.Require().NoError(err, s.errBody(err))
@@ -90,7 +116,7 @@ func (s *APISuite) todoTask(taskID string) {
 	s.Require().Equal(http.StatusOK, resp.StatusCode)
 }
 
-func (s *APISuite) doneTask(taskID string) {
+func (s *APISuiteActions) doneTask(taskID string) {
 	s.T().Helper()
 	resp, err := s.cli.DefaultApi.DoneTask(s.ctx, taskID)
 	s.Require().NoError(err, s.errBody(err))
@@ -98,7 +124,7 @@ func (s *APISuite) doneTask(taskID string) {
 	s.Require().Equal(http.StatusOK, resp.StatusCode)
 }
 
-func (s *APISuite) doneTaskWithError(taskID string, httpStatus int) {
+func (s *APISuiteActions) doneTaskWithError(taskID string, httpStatus int) {
 	s.T().Helper()
 	resp, err := s.cli.DefaultApi.DoneTask(s.ctx, taskID)
 	s.Require().Error(err)
@@ -106,7 +132,7 @@ func (s *APISuite) doneTaskWithError(taskID string, httpStatus int) {
 	s.Require().Equal(httpStatus, resp.StatusCode)
 }
 
-func (s *APISuite) cancelTask(taskID string) {
+func (s *APISuiteActions) cancelTask(taskID string) {
 	s.T().Helper()
 	resp, err := s.cli.DefaultApi.CancelTask(s.ctx, taskID)
 	s.Require().NoError(err, s.errBody(err))
@@ -114,7 +140,7 @@ func (s *APISuite) cancelTask(taskID string) {
 	s.Require().Equal(http.StatusOK, resp.StatusCode)
 }
 
-func (s *APISuite) cancelTaskWithError(taskID string, httpStatus int) {
+func (s *APISuiteActions) cancelTaskWithError(taskID string, httpStatus int) {
 	s.T().Helper()
 	resp, err := s.cli.DefaultApi.CancelTask(s.ctx, taskID)
 	s.Require().Error(err)
@@ -122,7 +148,7 @@ func (s *APISuite) cancelTaskWithError(taskID string, httpStatus int) {
 	s.Require().Equal(httpStatus, resp.StatusCode)
 }
 
-func (s *APISuite) updateTask(task openapicli.RespTask) {
+func (s *APISuiteActions) updateTask(task openapicli.RespTask) {
 	s.T().Helper()
 	opts := openapicli.UpdateOptions{
 		Text:   task.Text,
