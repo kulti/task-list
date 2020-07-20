b13950bfe85267021c316f8cb52b07ed6f3bc1c0
Aleksey Bakin <alexey.bakin@gmail.com>
2020-07-20 21:05:38 +0300

    chore: enable prettier

--- front/src/TaskEditor.ts
+++ front/src/TaskEditor.ts
@@ -1,73 +1,73 @@
 export enum TaskEditorFocus {
-    Text = 'text',
-    Points = 'points',
-    None = 'none'
+  Text = "text",
+  Points = "points",
+  None = "none",
 }
 
 export interface TaskEditorTask {
-    text: string;
-    points: number;
-    burnt?: number;
+  text: string;
+  points: number;
+  burnt?: number;
 }
 
 export function BuildTaskEditor(
-    applyFn: (text: string, points: string) => void,
-    cancelFn?: () => void,
-    resetDiv?: HTMLElement,
-    task?: TaskEditorTask,
-    focus = TaskEditorFocus.None
+  applyFn: (text: string, points: string) => void,
+  cancelFn?: () => void,
+  resetDiv?: HTMLElement,
+  task?: TaskEditorTask,
+  focus = TaskEditorFocus.None
 ): HTMLElement {
-    const taskTextInput = document.createElement('input') as HTMLInputElement;
-    taskTextInput.className = "text form-control";
-    taskTextInput.type = "text";
-    taskTextInput.placeholder = "Do new task";
+  const taskTextInput = document.createElement("input") as HTMLInputElement;
+  taskTextInput.className = "text form-control";
+  taskTextInput.type = "text";
+  taskTextInput.placeholder = "Do new task";
 
-    const taskPointsInput = document.createElement('input') as HTMLInputElement;
-    taskPointsInput.className = "points form-control";
-    taskPointsInput.type = "text";
-    taskPointsInput.placeholder = "0";
+  const taskPointsInput = document.createElement("input") as HTMLInputElement;
+  taskPointsInput.className = "points form-control";
+  taskPointsInput.type = "text";
+  taskPointsInput.placeholder = "0";
 
-    if (task) {
-        taskTextInput.value = task.text;
-        if (task.burnt !== undefined) {
-            taskPointsInput.value = task.burnt + "/"
+  if (task) {
+    taskTextInput.value = task.text;
+    if (task.burnt !== undefined) {
+      taskPointsInput.value = task.burnt + "/";
+    }
+    taskPointsInput.value += task.points;
+  }
+
+  if (focus !== TaskEditorFocus.None) {
+    const autofocusPoints = focus === TaskEditorFocus.Points;
+    const autofocusInput = autofocusPoints ? taskPointsInput : taskTextInput;
+    setTimeout(() => {
+      autofocusInput.focus();
+    }, 0);
+  }
+
+  const taskDiv = document.createElement("div") as HTMLDivElement;
+  taskDiv.className = "form-group task";
+  taskDiv.append(taskTextInput, taskPointsInput);
+
+  const handleKeyPress = (ev: KeyboardEvent) => {
+    switch (ev.key) {
+      case "Escape":
+        if (resetDiv) {
+          taskDiv.replaceWith(resetDiv);
+        } else {
+          taskTextInput.value = "";
+          taskPointsInput.value = "";
         }
-        taskPointsInput.value += task.points;
-    }
 
-    if (focus !== TaskEditorFocus.None) {
-        const autofocusPoints = (focus === TaskEditorFocus.Points)
-        const autofocusInput = autofocusPoints ? taskPointsInput : taskTextInput;
-        setTimeout(() => {
-            autofocusInput.focus()
-        }, 0);
-    }
-
-    const taskDiv = document.createElement('div') as HTMLDivElement;
-    taskDiv.className = "form-group task";
-    taskDiv.append(taskTextInput, taskPointsInput);
-
-    const handleKeyPress = (ev: KeyboardEvent) => {
-        switch (ev.key) {
-            case 'Escape':
-                if (resetDiv) {
-                    taskDiv.replaceWith(resetDiv)
-                } else {
-                    taskTextInput.value = ""
-                    taskPointsInput.value = ""
-                }
-
-                if (cancelFn) {
-                    cancelFn()
-                }
-                break;
-            case 'Enter':
-                applyFn(taskTextInput.value, taskPointsInput.value)
+        if (cancelFn) {
+          cancelFn();
         }
+        break;
+      case "Enter":
+        applyFn(taskTextInput.value, taskPointsInput.value);
     }
+  };
 
-    taskTextInput.onkeyup = handleKeyPress;
-    taskPointsInput.onkeyup = handleKeyPress;
+  taskTextInput.onkeyup = handleKeyPress;
+  taskPointsInput.onkeyup = handleKeyPress;
 
-    return taskDiv;
+  return taskDiv;
 }
--- front/tests/DropdownMenu.test.ts
+++ front/tests/DropdownMenu.test.ts
@@ -1,44 +1,47 @@
-describe('list elements', () => {
-    test.each`
-        type          | items
-        ${'todo'}     | ${Array('Done', 'Cancel', 'Delete')}
-        ${''}         | ${Array('Done', 'Todo', 'Cancel', 'Delete')}
-        ${'done'}     | ${Array('Delete')}
-        ${'canceled'} | ${Array('Delete')}
-    `(
-        'list "$type" should contains: $items',
-        ({ type, items }) => {
-            const BuildDropdownMenu = require('../src/DropdownMenu').BuildDropdownMenu
+describe("list elements", () => {
+  test.each`
+    type          | items
+    ${"todo"}     | ${Array("Done", "Cancel", "Delete")}
+    ${""}         | ${Array("Done", "Todo", "Cancel", "Delete")}
+    ${"done"}     | ${Array("Delete")}
+    ${"canceled"} | ${Array("Delete")}
+  `('list "$type" should contains: $items', ({ type, items }) => {
+    const BuildDropdownMenu = require("../src/DropdownMenu").BuildDropdownMenu;
 
-            const menuDiv = BuildDropdownMenu(type, {}, {}, {}, {})
+    const menuDiv = BuildDropdownMenu(type, {}, {}, {}, {});
 
-            expect(menuDiv.className).toEqual('dropdown-menu')
-            expect(menuDiv.childNodes).toHaveLength(items.length)
+    expect(menuDiv.className).toEqual("dropdown-menu");
+    expect(menuDiv.childNodes).toHaveLength(items.length);
 
-            items.forEach((item: string, i: number) => {
-                const menuItem = menuDiv.children[i] as HTMLDivElement
-                expect(menuItem.innerText).toEqual(item)
-            });
-        }
-    )
+    items.forEach((item: string, i: number) => {
+      const menuItem = menuDiv.children[i] as HTMLDivElement;
+      expect(menuItem.innerText).toEqual(item);
+    });
+  });
 });
 
-describe('list item actions', () => {
-    const fnMap = new Map<string, jest.Mock<any, any>>();
-    fnMap.set('Done', jest.fn())
-    fnMap.set('Todo', jest.fn())
-    fnMap.set('Cancel', jest.fn())
-    fnMap.set('Delete', jest.fn())
+describe("list item actions", () => {
+  const fnMap = new Map<string, jest.Mock<any, any>>();
+  fnMap.set("Done", jest.fn());
+  fnMap.set("Todo", jest.fn());
+  fnMap.set("Cancel", jest.fn());
+  fnMap.set("Delete", jest.fn());
 
-    const BuildDropdownMenu = require('../src/DropdownMenu').BuildDropdownMenu
-    const menuDiv = BuildDropdownMenu('', fnMap.get('Todo'), fnMap.get('Done'), fnMap.get('Cancel'), fnMap.get('Delete'))
+  const BuildDropdownMenu = require("../src/DropdownMenu").BuildDropdownMenu;
+  const menuDiv = BuildDropdownMenu(
+    "",
+    fnMap.get("Todo"),
+    fnMap.get("Done"),
+    fnMap.get("Cancel"),
+    fnMap.get("Delete")
+  );
 
-    expect(menuDiv.childNodes).toHaveLength(4)
+  expect(menuDiv.childNodes).toHaveLength(4);
 
-    menuDiv.childNodes.forEach((menuItem: HTMLDivElement) => {
-        it('click on ' + menuItem.innerText.toString(), () => {
-            menuItem.click()
-            expect(fnMap.get(menuItem.innerText)).toBeCalled()
-        });
+  menuDiv.childNodes.forEach((menuItem: HTMLDivElement) => {
+    it("click on " + menuItem.innerText.toString(), () => {
+      menuItem.click();
+      expect(fnMap.get(menuItem.innerText)).toBeCalled();
     });
+  });
 });
