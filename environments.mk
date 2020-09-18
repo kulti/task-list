d9279e881239af16310ed437a5bfda9459ce46f6
Aleksey Bakin <alexey.bakin@gmail.com>
2020-09-18 21:24:41 +0300

    chore(infra): new env.sh instead of deploy.sh

--- environments.mk
+++ environments.mk
@@ -4,15 +4,15 @@ up-tl-prod-deps: build-docker-tl-proxy build-docker-tl-front build-docker-tl-ser
 up-tl-dev-deps: build-docker-tl-proxy build-docker-tl-front build-docker-tl-live-reload build-docker-tl-migrate
 
 $(addprefix up-tl-, $(ENVIRONMENTS)): up-tl-%: % up-tl-%-deps
-	./scripts/deploy.sh $< up
+	./deployments/env.sh $< up
 
 $(addprefix down-tl-, $(ENVIRONMENTS)): down-tl-%: %
-	./scripts/deploy.sh $< down
+	./deployments/env.sh $< down
 
 $(addprefix ps-tl-, $(ENVIRONMENTS)): ps-tl-%: %
-	./scripts/deploy.sh $< ps
+	./deployments/env.sh $< ps
 
 $(addprefix logs-tl-, $(ENVIRONMENTS)): logs-tl-%: %
-	./scripts/deploy.sh $< logs
+	./deployments/env.sh $< logs
 
 .PHONY: $(ENVIRONMENTS)
